b4f2d8e7ee289d75af7893d7f2d8b05fb823b502
Koemy <francine.pns@gmail.com>
2022-12-29 11:42:15 -0300

    Add autenticator to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import Navbar from './components/Navbar';
 import Home from "./pages/Home";
 import {Route, Routes} from "react-router-dom";
+import {AuthContextProvider} from "./context/AuthContext";
 
 function App() {
-  return (
-    <div>
-        <Navbar/>
-        <Routes>
-            <Route path='/' element={<Home/>} />
-        </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <AuthContextProvider>
+                <Navbar/>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                </Routes>
+            </AuthContextProvider>
+        </div>
+    );
 }
 
 export default App;
