5212344b81c65d48bb678f359fc71e76c81e10d8
Koemy <francine.pns@gmail.com>
2022-12-29 11:41:37 -0300

    Create authentication function

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,13 +1,45 @@
 import {createContext, useContext, useEffect, useState} from "react";
+import {auth} from '../firebase';
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged} from 'firebase/auth';
 
 const AuthContext = createContext(0)
 
 interface AuthComponent {
-    children: string,
+    children: any,
 }
 
 export function AuthContextProvider({children}: AuthComponent){
+    const [user, setUser] = useState({});
+    function signUp(email: string, password: string)  {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    function logIn (email: string, password: string) {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    function logOut () {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            // @ts-ignore
+            setUser(currentUser)
+        })
+        return () => {
+            unsubscribe()
+        }
+    })
+
     return (
-        <Auth
+        // @ts-ignore
+        <AuthContext.Provider value={{signUp, logIn, logOut, user}}>
+            {children}
+        </AuthContext.Provider>
     )
+}
+
+export function UserAuth() {
+    return useContext(AuthContext)
 }
