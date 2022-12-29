75a7fd71281fda5390044d8196e7eed3ac0e7963
Koemy <francine.pns@gmail.com>
2022-12-29 15:29:25 -0300

    Create login system with firebase

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,48 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {UserAuth} from "../context/AuthContext";
+import {useState} from "react";
 
-function Login () {
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('')
+    //@ts-ignore
+    const {user, logIn} = UserAuth();
+    const navigate = useNavigate();
+    // @ts-ignore
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setError('')
+        try {
+            await logIn(email, password)
+            navigate('/')
+        } catch (error) {
+            // @ts-ignore
+            setError(error.message)
+        }
+    }
     return (
         <div>
             <div className="w-full h-screen">
-                <img className="hidden sm:block absolute w-full h-full object-cover" src="https://assets.nflxext.com/ffe/siteui/vlv3/5e48e7b6-350d-48d9-96d6-de8ca173c89f/42b5dfb2-43c7-478f-a264-4a16c1f13ccb/BR-pt-20221219-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt=""/>
+                <img className="hidden sm:block absolute w-full h-full object-cover"
+                     src="https://assets.nflxext.com/ffe/siteui/vlv3/5e48e7b6-350d-48d9-96d6-de8ca173c89f/42b5dfb2-43c7-478f-a264-4a16c1f13ccb/BR-pt-20221219-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+                     alt=""/>
                 <div className="bg-black/60 fixed top-0 left-0 w-full h-screen">
                 </div>
                 <div className="fixed w-full px-4 py-24 z-50">
                     <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
                         <div className="max-w-[320px] mx-auto py-16">
                             <h1 className="text-3xl font-bold">Sign In</h1>
-                            <form className="w-full flex flex-col py-4">
-                                <input className="p-3 my-2 bg-gray-700 rounded" placeholder="Email" type="email" autoComplete="email"/>
-                                <input className="p-3 my-2 bg-gray-700 rounded" placeholder="Password" type="password" autoComplete="current-password"/>
+                            {
+                                error ? <p className="bg-orange-600 rounded p-3 mt-3">{error}</p> : null
+                            }
+                            <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
+                                <input onChange={(e) => setEmail(e.target.value)}
+                                       className="p-3 my-2 bg-gray-700 rounded" placeholder="Email" type="email"
+                                       autoComplete="email"/>
+                                <input onChange={(e) => setPassword(e.target.value)}
+                                       className="p-3 my-2 bg-gray-700 rounded" placeholder="Password" type="password"
+                                       autoComplete="current-password"/>
                                 <button className="bg-red-600 py-3 my-6 rounded font-bold">Sing In</button>
                                 <div className="flex justify-between items-center text-sm text-gray-600">
                                     <p><input className="mr-2" type="checkbox"/> Remember me</p>
