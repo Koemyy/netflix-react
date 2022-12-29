a1feacda25375ffb16300ef14a8a23313ad9e3bf
Koemy <francine.pns@gmail.com>
2022-12-29 12:24:39 -0300

    Add links to other other pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,18 @@
-function Navbar () {
+import {Link} from "react-router-dom";
+
+function Navbar() {
     return (
         <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
-            <h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
+            <Link to="/">
+                <h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
+            </Link>
             <div>
-                <button className="text-white pr-4">Sing In</button>
-                <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">Sing Up</button>
+                <Link to="/login">
+                    <button className="text-white pr-4">Sing In</button>
+                </Link>
+                <Link to="/singup">
+                    <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">Sing Up</button>
+                </Link>
             </div>
         </div>
     )
